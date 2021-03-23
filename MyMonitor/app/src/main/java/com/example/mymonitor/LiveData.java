b2610e5f39fb09775995a7db4cc9f34fb1253280
aqeelahlam <aqeelahlam@gmail.com>
2021-03-23 14:19:56 +0530

    feature/ Added latest data read from Arduino

--- MyMonitor/app/src/main/java/com/example/mymonitor/LiveData.java
+++ MyMonitor/app/src/main/java/com/example/mymonitor/LiveData.java
@@ -14,15 +14,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mymonitor.provider.Reading;
 import com.example.mymonitor.provider.FirebaseViewModel;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -38,7 +43,15 @@ public class LiveData extends Fragment {
     TextView sp_reading;
     TextView sp_avg;
     String userDevice;
+
+    Object[] keys;
     Random rand = new Random();
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("ARDUINO1/");
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -70,12 +83,11 @@ public class LiveData extends Fragment {
         sp_avg = view.findViewById(R.id.card_sp_average_reading);
 
         Button button = (Button) view.findViewById(R.id.button_livedata);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                randomDataGenerator();
+            public void onClick(View v) {
+//                randomDataGenerator();
+                DataFromFirebase();
             }
         });
 
@@ -120,12 +132,63 @@ public class LiveData extends Fragment {
 
     }
 
-    public void randomDataGenerator(){
+    private void DataFromFirebase() {
+
+        final Reading patientReading = new Reading();
+        final HashMap<String, Reading> readingHashMap = new HashMap<>();
 
 
-        int n = rand.nextInt(10);
-        mLiveData.push().setValue(new Reading(Integer.toString(90+n), Integer.toString(90+n), Integer.toString(35+n)));
+        ref.child("data").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterator<DataSnapshot> items = snapshot.getChildren().iterator();
+                while (items.hasNext()){
+                    DataSnapshot item = items.next();
+                    patientReading.setHeartRate(item.child("Heart Rate(BPM)").getValue().toString());
+                    patientReading.setSP02(item.child("SP02(%)").getValue().toString());
+                    patientReading.setTemperature(item.child("Temperature (°C)").getValue().toString());
+                    readingHashMap.put((item.child("Time").getValue().toString()), patientReading);
+
+                }
+
+                keys = readingHashMap.keySet().toArray();
+
+                Object last = keys[keys.length-1];
+
+                Reading test = readingHashMap.get(last);
+
+                temp_reading.setText(test.getTemperature());
+                heart_reading.setText(test.getHeartRate());
+//                heart_avg
+                sp_reading.setText(test.getSP02());
+
+                }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
     }
+
+//    public void randomDataGenerator(){
+//
+//
+//        int n = rand.nextInt(10);
+//        mLiveData.push().setValue(new Reading(Integer.toString(90+n), Integer.toString(90+n), Integer.toString(35+n)));
+//    }
 }
 
 
